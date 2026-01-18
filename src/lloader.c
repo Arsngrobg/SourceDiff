59a46a8552f65040595f83fe4a45aab31f4c3a04
James Armstrong <arsngrobg@gmail.com>
2026-01-18 19:08:43 +0000

    simple control flow for language parser compilation

--- src/lloader.c
+++ src/lloader.c
@@ -12,13 +12,18 @@
 // Version: v1.0                                                                                                      //
 // ================================================================================================================== //
 
+#include <stddef.h>
 #include <stdbool.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+#include "libtcc.h"
 
 #define LANGUAGE_DIRECTORY "languages"
 #define LANGUAGE_CACHE     "languages/.bin"
 
 /**
- * <p>Compiles the <i>Tree Sitter</i> language parser <i>(+source)</i>.</p>
+ * <p>Compiles the <i>Tree Sitter</i> language parser <i>(+scanners)</i>.</p>
  * <p>The <c>language_id</c> must be a directory in the <c>LANGUAGE_DIRECTORY</c>, if not this function returns
  *    <c>false</c>.
  * </p>
@@ -38,3 +43,23 @@ bool SD_CompileLanguage(const char *language_id);
  * @author             Arsngrobg
  */
 bool SD_LoadLanguage   (const char *language_id);
+
+bool SD_CompileLanguage(const char *language_id) {
+    if (language_id == NULL) return false;
+
+    // format path string
+    char path[260];
+    if (sprintf(path, LANGUAGE_CACHE"/%s-lang.dll", language_id) == -1) { // TODO: .DLL & .SO support
+        return false;
+    }
+
+    TCCState *cc = tcc_new();
+
+    // TODO: add tree sitter parser (+scanner)
+
+    tcc_set_output_type(cc, TCC_OUTPUT_DLL);
+    tcc_output_file(cc, path);
+
+    tcc_delete(cc);
+    return true;
+}
