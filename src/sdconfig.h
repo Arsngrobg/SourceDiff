cca9c4ff304089f92438625bd51e3b71d28bf9e6
James Armstrong <arsngrobg@gmail.com>
2026-04-17 14:08:50 +0100

    fix: remove redundant types

--- src/sdconfig.h
+++ src/sdconfig.h
@@ -16,8 +16,8 @@
 #ifndef SDCONFIG_H
 #define SDCONFIG_H
 
-#include <stdint.h>
 #include <stdbool.h>
+#include "sdoptions.h"
 
 typedef enum {
     SD_MODE_NONE     = 0, // srcdiff [options]
@@ -25,18 +25,12 @@ typedef enum {
     SD_MODE_REGISTER = 2  // srcdiff [options] register <name>  <dir>
 } SD_Mode;
 
-typedef enum {
-    SD_FLAG_HELP    = 1 << 0, // srcdiff --help    [mode]
-    SD_FLAG_VERSION = 1 << 1, // srcdiff --version [mode]
-    SD_FLAG_VERBOSE = 1 << 2  // srcdiff -v        [mode]
-} SD_Flag;
-
 typedef struct {
-    char    *exec_name; // the name of the executable that hosts SourceDiff
-    char    *output;    // the output file
-    char    *args[2];   // the argument buffer for modes
-    SD_Mode  mode;      // the current mode
-    uint8_t  options;   // the boolean flags that enable certain options
+    char         *exec_name; // the name of the executable that hosts SourceDiff
+    char         *output;    // the output file
+    char         *args[2];   // the argument buffer for modes
+    SD_Mode       mode;      // the current mode
+    SD_OptionSet  options;   // the options
 } SD_Config;
 
 #endif // SDCONFIG_H
--- src/sdoptions.h
+++ src/sdoptions.h
@@ -24,14 +24,13 @@
     X(VERBOSE, "-v"       )
 
 typedef enum {
-#define X(id, str) SD_OPTION_##id,
+#define X(id, str) SD_OPTION_##id = 1 << __COUNTER__,
     SD_OPTIONS
 #undef X
     SD_OPTION_COUNT
 } SD_Option;
 
 typedef uint8_t SD_OptionSet; // all our flags fit into this mask
-
 _Static_assert(SD_OPTION_COUNT <= (sizeof(SD_OptionSet) * 8), "too many options that cannot fit inside the bits of SD_OptionSet");
 
 #endif // SDOPTIONS_h
