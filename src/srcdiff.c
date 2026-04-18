4dd9be227e7755ed9dea884144ea205cf58cf6c5
James Armstrong <arsngrobg@gmail.com>
2026-04-18 03:58:41 +0100

    feat(cli): proper string slicing

--- src/srcdiff.c
+++ src/srcdiff.c
@@ -24,39 +24,52 @@
 #include "sdconfig.h"
 #include "sdoptions.h"
 
-#define SD_HELP_STRING                                                                 \
-    "SourceDiff v"SD_VERSION" - "SD_DESCRIPTION"\n"                                    \
-    "Copyright (c) 2025 James Armstrong\n"                                             \
-    "\n"                                                                               \
-    "Usage:\n"                                                                         \
-    "  %s diff     <file1> <file2>   The difference between <file1> and <file2>\n"     \
-    "  %s register <name>  <dir>     Registers a new language (C compiler required)\n" \
-    "Options:\n"                                                                       \
-    "  --help      Display this information\n"                                         \
-    "  --version   Display version information for SourceDiff\n"                       \
-    "  -v          Display diagnostics during analysis\n"                              \
-    "  -o <file>   Output digestible, structured analysis results to the <file>\n"     \
-    "\n"                                                                               \
+#if !defined(SD_VERSION) || !defined(SD_DESCRIPTION) || !defined(SD_REPO)
+#error SD_VERSION, SD_DESCRIPTION, or SD_REPO are not defined!
+#endif // !defined(SD_VERSION) || !defined(SD_DESCRIPTION) || !defined(SD_REPO)
+
+#define SD_HELP_STRING                                                                   \
+    "SourceDiff v"SD_VERSION" - "SD_DESCRIPTION"\n"                                      \
+    "Copyright (c) 2025 James Armstrong\n"                                               \
+    "\n"                                                                                 \
+    "Usage:\n"                                                                           \
+    "  %.*s diff     <file1> <file2>   The difference between <file1> and <file2>\n"     \
+    "  %.*s register <name>  <dir>     Registers a new language (C compiler required)\n" \
+    "Options:\n"                                                                         \
+    "  --help      Display this information\n"                                           \
+    "  --version   Display version information for SourceDiff\n"                         \
+    "  -v          Display diagnostics during analysis\n"                                \
+    "  -o <file>   Output digestible, structured analysis results to the <file>\n"       \
+    "\n"                                                                                 \
     "For more information: "SD_REPO"\n"
 
-char *sd_strip_ext(char *file) {
+typedef struct {
+    const char *str;
+    uint64_t    start;
+    uint64_t    length;
+} SD_StringSlice;
+
+void sd_strip_ext(const char *file, SD_StringSlice *slice) {
     assert(file != NULL); assert(strlen(file) != 0);
 
-    int32_t idx = strlen(file) - 1;
-    while (idx >= 0) {
-        if (file[idx] == '.') {
-            file[idx] = '\0';
+    slice->str    = file;
+    slice->start  = 0;
+    slice->length = strlen(file);
+    while (slice->length > 0) {
+        if (slice->str[slice->length - 1] == '.') {
+            slice->length--;
             break;
         }
 
-        idx--;
+        slice->length--;
     }
-
-    return file;
+    (void)0;
 }
 
 int32_t main(int32_t argc, char *argv[]) {
     (void) argc;
-    printf(SD_HELP_STRING, sd_strip_ext(argv[0]), argv[0]);
+    SD_StringSlice noext;
+    sd_strip_ext(argv[0], &noext);
+    printf(SD_HELP_STRING, (int) noext.length, noext.str, (int) noext.length, noext.str);
     return 0;
 }
