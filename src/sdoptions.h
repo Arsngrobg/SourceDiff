177b88b34d17ef25d21ef7d7473502b67f5e8e16
James Armstrong <arsngrobg@gmail.com>
2026-04-18 03:57:33 +0100

    chore(cli): remove `SD_OptionType` enum

--- src/sdoptions.h
+++ src/sdoptions.h
@@ -23,17 +23,10 @@
 #define SD_OPTION_STRING_VERBOSE "-v"
 
 typedef enum {
-    SD_OPTION_TYPE_NONE    = 0,
-    SD_OPTION_TYPE_HELP    = 1,
-    SD_OPTION_TYPE_VERSION = 2,
-    SD_OPTION_TYPE_VERBOSE = 3
-} SD_OptionType;
-
-typedef enum {
-    SD_OPTION_BIT_HELP    = 1 << SD_OPTION_TYPE_HELP,
-    SD_OPTION_BIT_VERSION = 1 << SD_OPTION_TYPE_VERSION,
-    SD_OPTION_BIT_VERBOSE = 1 << SD_OPTION_TYPE_VERBOSE
-} SD_OptionBit;
+    SD_OPTION_BIT_HELP    = 1 << 0,
+    SD_OPTION_BIT_VERSION = 1 << 1,
+    SD_OPTION_BIT_VERBOSE = 1 << 2
+} SD_Option;
 
 // 00000vVH
 typedef uint8_t SD_OptionSet;
