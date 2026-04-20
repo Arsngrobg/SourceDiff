2d544a77ab115b32e4e2bc798de13ad42bf55166
James Armstrong <arsngrobg@gmail.com>
2026-04-20 16:54:31 +0100

    refactor: merge `sdmode.h` & `sdoptions.h` into `sdconfig.h`

--- src/sdconfig.h
+++ src/sdconfig.h
@@ -11,20 +11,42 @@
 //   ▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░▌          ▐░▌
 //   ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀   ▀▀▀▀▀▀▀▀▀▀▀  ▀            ▀
 //
-//   The configuration data of the application through its CLI interface
+//   The configuration data of the application through its CLI interface.
+//   Consists of: modes & global options
 
 #ifndef SDCONFIG_H
 #define SDCONFIG_H
 
-#include "sdoptions.h"
-#include "sdmode.h"
+#include "sdstr.h"
+
+#define SD_OPTION_STRING_HELP    "--help"
+#define SD_OPTION_STRING_VERSION "--version"
+#define SD_OPTION_STRING_VERBOSE "-v"
+#define SD_MODE_DIFF_STRING      "diff"
+#define SD_MODE_REGISTER_STRING  "register"
+#define SD_MODE_ARG_MAX          2
+
+// 00000vVH
+typedef uint8_t SD_OptionSet;
+
+typedef enum {
+    SD_OPTION_HELP    = 1 << 0,
+    SD_OPTION_VERSION = 1 << 1,
+    SD_OPTION_VERBOSE = 1 << 2
+} SD_Option;
+
+typedef enum {
+    SD_MODE_NONE     = 0,
+    SD_MODE_DIFF     = 1,
+    SD_MODE_REGISTER = 2
+} SD_Mode;
 
 typedef struct {
-    char         *exec_name; // the name of the executable that hosts SourceDiff
-    char         *output;    // the output file
-    char         *args[2];   // the argument buffer for modes
-    SD_Mode       mode;      // the current mode
-    SD_OptionSet  options;   // the global options
+    SD_StringSlice  exec_name;             // the name of the executable that hosts SourceDiff
+    char           *output;                // the output file
+    char           *args[SD_MODE_ARG_MAX]; // the argument buffer for modes
+    SD_Mode         mode;                  // the current mode
+    SD_OptionSet    options;               // the global options
 } SD_Config;
 
 #endif // SDCONFIG_H
