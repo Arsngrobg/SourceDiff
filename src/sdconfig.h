e42642ba59c23cc6130e6b810f9ea5ef341cde23
James Armstrong <arsngrobg@gmail.com>
2026-04-20 21:35:46 +0100

    refactor: using better `sdstring.h` types

--- src/sdconfig.h
+++ src/sdconfig.h
@@ -12,41 +12,41 @@
 //   ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀   ▀▀▀▀▀▀▀▀▀▀▀  ▀            ▀
 //
 //   The configuration data of the application through its CLI interface.
-//   Consists of: modes & global options
+//   It contains the configured state of SourceDiff through its supplied arguments.
 
 #ifndef SDCONFIG_H
 #define SDCONFIG_H
 
-#include "sdstr.h"
+#include <stdint.h>
 
-#define SD_OPTION_STRING_HELP    "--help"
-#define SD_OPTION_STRING_VERSION "--version"
-#define SD_OPTION_STRING_VERBOSE "-v"
-#define SD_MODE_DIFF_STRING      "diff"
-#define SD_MODE_REGISTER_STRING  "register"
-#define SD_MODE_ARG_MAX          2
+#include "sdstring.h"
 
-// 00000vVH
-typedef uint8_t SD_OptionSet;
-
+// The modes of SourceDiff
 typedef enum {
-    SD_OPTION_HELP    = 1 << 0,
-    SD_OPTION_VERSION = 1 << 1,
-    SD_OPTION_VERBOSE = 1 << 2
-} SD_Option;
-
-typedef enum {
-    SD_MODE_NONE     = 0,
-    SD_MODE_DIFF     = 1,
-    SD_MODE_REGISTER = 2
+    SD_MODE_NONE     = 0, // ./srcdiff          ...
+    SD_MODE_DIFF     = 1, // ./srcdiff diff     ...
+    SD_MODE_REGISTER = 2  // ./srcdiff register ...
 } SD_Mode;
 
+// The global options of SourceDiff - each representing their bit they configure
+typedef enum {
+    SD_OPTION_HELP    = 1 << 0, // ./srcdiff --help
+    SD_OPTION_VERSION = 1 << 1, // ./srcdiff --version
+    SD_OPTION_VERBOSE = 1 << 2, // ./srcdiff -v        ...
+    SD_OPTION_OUTPUT  = 1 << 3  // ./srcdiff -o <file> ...
+} SD_Option;
+
+// The bits we use to denote enabled or disabled options
+// 0000ovVH
+typedef uint8_t SD_OptionSet;
+
+// The configuration of SourceDiff
 typedef struct {
-    SD_StringSlice  exec_name;             // the name of the executable that hosts SourceDiff
-    char           *output;                // the output file
-    char           *args[SD_MODE_ARG_MAX]; // the argument buffer for modes
-    SD_Mode         mode;                  // the current mode
-    SD_OptionSet    options;               // the global options
+    SD_StringView exec;    // the name of the executable hosting SourceDiff
+    SD_StringView args[2]; // the regular arguments supplied to the application
+    SD_StringView output;  // the output file
+    SD_Mode       mode;    // the set mode
+    SD_OptionSet  options; // the global options
 } SD_Config;
 
 #endif // SDCONFIG_H
