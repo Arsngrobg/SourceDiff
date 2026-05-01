d577f35b5ab6bb73208e6d3f0b52a94444c06644
James Armstrong <arsngrobg@gmail.com>
2026-05-01 14:26:28 +0100

    feat: add helper functions

--- src/sdstring.h
+++ src/sdstring.h
@@ -15,18 +15,23 @@
 #include <stddef.h>
 
 #define SD_STRING_FORMAT(s) \
-    ((int)(s).length),((s).buf)
+    ((int)(s).length),((s).bytes)
 
 // A length-based, owned string
 typedef struct {
     size_t length;
-    char   buf[];
+    char   bytes[];
 } SD_String;
 
 // A view into a string (either char* or SD_String)
 typedef struct {
     size_t      length;
-    const char *buf;
+    const char *bytes;
 } SD_StringView;
+
+SD_String     *sd_str_ofstrc (const char *cstr);
+SD_String     *sd_str_substr (const char *cstr, size_t offset, size_t length);
+SD_StringView  sd_str_viewof (SD_String *str, size_t offset, size_t length);
+SD_StringView  sd_str_viewofc(const char *cstr, size_t offset, size_t length);
 
 #endif // SDSTRING_H
