d4d536fc088e6c2c633c842a8f5f8ce56c74346c
James Armstrong <arsngrobg@gmail.com>
2026-05-05 19:04:07 +0100

    fix: reflect naming

--- src/srcdiff.c
+++ src/srcdiff.c
@@ -17,9 +17,7 @@
 #include "tree_sitter/api.h"
 #include "libcc.h"
 
-#include "sd_debug.h"
-#include "sd_config.h"
-#include "sd_modes.h"
+#include "srcdiff.h"
 
 #if !defined(SD_VERSION) || !defined(SD_DESCRIPTION) || !defined(SD_DOCS)
 #error SD_VERSION, SD_DESCRIPTION, or SD_DOCS are not defined!
@@ -44,117 +42,27 @@
     "\n"                                                                                    \
     "For more information: "SD_DOCS"\n"
 
-int32_t SD_parse_args(int32_t argc, const char *argv[], SD_Config *cfg) {
-    assert(argc >= 0); assert(argv != NULL);
-
-    int32_t status = EXIT_SUCCESS;
-
-    cfg->exec = argv[0];
-    cfg->mode = SD_MODE_NONE;
-
-    for (size_t arg = 1; arg < (size_t) argc; arg++) {
-        SD_DEBUG_LOGF("Processing command-line argument '%s'", argv[arg]);
-
-        // modes
-        if (strcmp(argv[arg], "diff") == 0) {
-            cfg->mode = SD_MODE_DIFF;
-            if ((arg + 2) >= (size_t) argc) {
-                status = EXIT_FAILURE;
-                fprintf(stderr, "%s: \x1b[1;31merror:\x1b[0m missing files to diff with\n", cfg->exec);
-                continue;
-            }
-
-            cfg->args[0] = argv[++arg];
-            SD_DEBUG_LOGF("diff ARG[0] = '%s'", cfg->args[0]);
-            cfg->args[1] = argv[++arg];
-            SD_DEBUG_LOGF("diff ARG[1] = '%s'", cfg->args[1]);
-        }
-        else if (strcmp(argv[arg], "analyse") == 0) {
-            cfg->mode = SD_MODE_ANALYSE;
-            if ((arg + 1) == (size_t) argc) {
-                status = EXIT_FAILURE;
-                fprintf(stderr, "%s: \x1b[1;31merror:\x1b[0m missing glob pattern\n", cfg->exec);
-                continue;
-            }
-
-            cfg->args[0] = argv[++arg];
-            SD_DEBUG_LOGF("analyse ARG[0] = '%s'", cfg->args[0]);
-        }
-        else if (strcmp(argv[arg], "lint") == 0) {
-            cfg->mode = SD_MODE_LINT;
-            if ((arg + 1) == (size_t) argc) {
-                status = EXIT_FAILURE;
-                fprintf(stderr, "%s: \x1b[1;31merror:\x1b[0m missing glob pattern\n", cfg->exec);
-                continue;
-            }
-
-            cfg->args[0] = argv[++arg];
-            SD_DEBUG_LOGF("lint ARG[0] = '%s'", cfg->args[0]);
-        }
-        else if (strcmp(argv[arg], "register") == 0) {
-            cfg->mode = SD_MODE_REGISTER;
-            if ((arg + 2) >= (size_t) argc) {
-                status = EXIT_FAILURE;
-                fprintf(stderr, "%s: \x1b[1;31merror:\x1b[0m missing language name and sources\n", cfg->exec);
-                continue;
-            }
-
-            cfg->args[0] = argv[++arg];
-            SD_DEBUG_LOGF("register ARG[0] = '%s'", cfg->args[0]);
-            cfg->args[1] = argv[++arg];
-            SD_DEBUG_LOGF("register ARG[1] = '%s'", cfg->args[1]);
-        }
-
-        // options
-        else if (strcmp(argv[arg], "--help") == 0) {
-            cfg->options |= SD_OPTION_HELP;
-        }
-        else if (strcmp(argv[arg], "--version") == 0) {
-            cfg->options |= SD_OPTION_VERSION;
-        }
-        else if (strcmp(argv[arg], "--list-languages") == 0) {
-            cfg->options |= SD_OPTION_LIST_LANGUAGES;
-        }
-        else if (strcmp(argv[arg], "-v") == 0) {
-            cfg->options |= SD_OPTION_VERBOSE;
-        }
-        else if (strcmp(argv[arg], "-o") == 0) {
-            cfg->options |= SD_OPTION_OUTPUT;
-            if ((arg + 1) == (size_t) argc) {
-                status = EXIT_FAILURE;
-                fprintf(stderr, "%s: \x1b[1;31merror:\x1b[0m missing filename after '-o'\n", cfg->exec);
-                continue;
-            }
-
-            SD_DEBUG_LOGF("Processing sub-argument of '-o' ('%s')", cfg->exec);
-            cfg->output = argv[++arg];
-        }
-    }
-
-    return status;
-}
-
-int32_t SD_exec(const SD_Config *cfg) {
-    assert(cfg != NULL);
+int32_t SD_Exec(void) {
+    assert(SD_CLArgsParsed());
 
     int32_t status = EXIT_SUCCESS;
 
     // these arguments have higher priority
-    if ((cfg->options & SD_OPTION_HELP) != 0) {
+    if (SD_IsOptionEnabled(SD_OPTION_HELP)) {
         fprintf(
             stdout,
             SD_HELP_STRING,
-            cfg->exec, cfg->exec, cfg->exec, cfg->exec
+            SD_GetExecName(), SD_GetExecName(), SD_GetExecName(), SD_GetExecName()
         );
         goto short_circuit;
-    } else if ((cfg->options & SD_OPTION_VERSION) != 0) {
+    } else if (SD_IsOptionEnabled(SD_OPTION_VERSION)) {
         fprintf(stdout, "v%s\n", SD_VERSION);
         goto short_circuit;
-    } else if ((cfg->options & SD_OPTION_LIST_LANGUAGES) != 0) {
+    } else if (SD_IsOptionEnabled(SD_OPTION_LIST_LANGUAGES)) {
         struct dirent *entry;
         DIR *dir = opendir("languages");
         if (dir == NULL) {
-            printf("%s: no languages registered\n", cfg->exec);
+            printf("%s: no languages registered\n", SD_GetExecName());
             status = EXIT_SUCCESS;
             goto short_circuit;
         }
@@ -171,7 +79,7 @@ int32_t SD_exec(const SD_Config *cfg) {
         rewinddir(dir);
 
         if (amount > 0) {
-            printf("%s: registered languages: \033[1;32m%lld\x1b[0m\n", cfg->exec, amount);
+            printf("%s: registered languages: \033[1;32m%lld\x1b[0m\n", SD_GetExecName(), amount);
             while ((entry = readdir(dir)) != NULL) {
                 char *dot = strrchr(entry->d_name, '.');
                 if (dot == NULL || strcmp(dot+1, CC_SHARED_LIB_EXT) != 0) {
@@ -189,25 +97,29 @@ int32_t SD_exec(const SD_Config *cfg) {
         goto short_circuit;
     }
 
-    switch (cfg->mode) {
+    switch (SD_GetMode()) {
         case SD_MODE_NONE:
-            fprintf(stderr, "%s: \x1b[1;31merror:\x1b[0m no mode specified\n", cfg->exec);
+            fprintf(stderr, "%s: \x1b[1;31merror:\x1b[0m no mode specified\n", SD_GetExecName());
             status = EXIT_FAILURE;
             break;
         case SD_MODE_DIFF:
-            fprintf(stderr, "%s: \x1b[1;31merror:\x1b[0m 'diff' mode not implemented (TODO)\n", cfg->exec);
-            status = EXIT_FAILURE; // SD_mode_diff(cfg);
+            SD_LOG("Entering DIFF mode");
+            fprintf(stderr, "%s: \x1b[1;31merror:\x1b[0m 'diff' mode not implemented (TODO)\n", SD_GetExecName());
+            status = EXIT_FAILURE; // SD_ExecDiff();
             break;
         case SD_MODE_ANALYSE:
-            fprintf(stderr, "%s: \x1b[1;31merror:\x1b[0m 'analyse' mode not implemented (TODO)\n", cfg->exec);
-            status = EXIT_FAILURE; // SD_mode_analyse(cfg);
+            SD_LOG("Entering ANALYSE mode");
+            fprintf(stderr, "%s: \x1b[1;31merror:\x1b[0m 'analyse' mode not implemented (TODO)\n", SD_GetExecName());
+            status = EXIT_FAILURE; // SD_ExecAnalyse();
             break;
         case SD_MODE_LINT:
-            fprintf(stderr, "%s: \x1b[1;31merror:\x1b[0m 'lint' mode not implemented (TODO)\n", cfg->exec);
-            status = EXIT_FAILURE; // SD_mode_lint(cfg);
+        SD_LOG("Entering LINT mode");
+            fprintf(stderr, "%s: \x1b[1;31merror:\x1b[0m 'lint' mode not implemented (TODO)\n", SD_GetExecName());
+            status = EXIT_FAILURE; // SD_ExecLint();
             break;
         case SD_MODE_REGISTER:
-            status = SD_mode_register(cfg);
+            SD_LOG("Entering REGISTER mode");
+            status = SD_ExecRegister();
             break;
     }
 
@@ -216,6 +128,8 @@ short_circuit:
 }
 
 int32_t main(int32_t argc, const char *argv[]) {
-    SD_Config cfg;
-    return SD_parse_args(argc, argv, &cfg) | SD_exec(&cfg);
+    if (!SD_ParseCLArgs(argc, argv)) {
+        return EXIT_FAILURE;
+    }
+    return SD_Exec();
 }
